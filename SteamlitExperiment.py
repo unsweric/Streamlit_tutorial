58394b5cd264738c1db256c10417624a86518cec
Eric Yan <yanhuan25@gmail.com>
2023-04-30 12:44:30 +1000

    Update SteamlitExperiment.py

--- SteamlitExperiment.py
+++ SteamlitExperiment.py
@@ -18,8 +18,8 @@ st.caption("Eric this is the caption")
 st.code("Eric x=2021")
 st.latex(r''' a+a r^1+a r^2+a r^3 ''')
 
-st.image("logan.jpg")
-st.video("logan.MOV")
+#st.image("logan.jpg")
+#st.video("logan.MOV")
 
 st.checkbox('yes')
 st.button('Click')
